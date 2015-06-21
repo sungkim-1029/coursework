d845ea9a4bb3f05dba9ea85f6820eb109990e091
sungkim-1029 <sungkim-1029@users.noreply.github.com>
2015-06-21 00:30:44 -0400

    Rename class name

---
+++ cs437/src/project/ProjectFinal.java
@@ -0,0 +1,15 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class ProjectFinal extends JFrame {
+	private static final String TITLE = "Project Tic Tac Toe";
+	private static final int WIDTH = 450;
+	private static final int HEIGHT = 450;
+
+	
+	public static void main(String[] args) {
+
+	}
+
+}

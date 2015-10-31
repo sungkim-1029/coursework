736d0be63989da5123951c5e05a6261cd923928c
sungkim-1029 <sungkim-1029@users.noreply.github.com>
2015-10-30 22:40:28 -0400

    Added comments for searching patterns

--- cmsc433/hw/stringSearch.py
+++ cmsc433/hw/stringSearch.py
@@ -2,8 +2,11 @@
 import re
 source = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America."
 
+# all words that end with "ty"
 pattern1 = re.findall(r"\b\w*ty\b", source)
+# all words that begin with "p"
 pattern2 = re.findall(r"\bp\w+\b", source, re.I)
+#all occurances of the word "the"
 pattern3 = re.findall(r"\bthe\b", source)
 
 print(pattern1)
